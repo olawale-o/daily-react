151dfb1c3aee06599341db0d336d56d87d932c02
olawale-o <omoogunolawale@gmail.com>
2024-02-04 03:44:41 +0100

    Update Calendar

--- src/components/Calendar/01/Calendar.jsx
+++ src/components/Calendar/01/Calendar.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-import { eachDayOfInterval, endOfMonth, format, startOfMonth, isToday, getDay, getDate } from "date-fns";
+import { eachDayOfInterval, endOfMonth, format, startOfMonth, isToday, getDay, getDate, isSameDay } from "date-fns";
 
 import { DAYS_OF_THE_WEEK } from "../constants/constants";
 
 import style from "./style.module.css";
+import { bookings } from "./data";
 
 const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
 
@@ -26,17 +27,22 @@ const CalendarNextDays = ({ lastDayOfMonth }) => {
   );
 };
 
+console.log(bookings);
 const CalendarDays = ({ days }) => {
+  const isBooked = (day) => {
+    return bookings.includes(day.getTime());
+  };
+
   return (
     <>
       {days.map((day, index) => (
-        <li key={index} className={`${isToday(day) ? style.today : ""}`}>
+        <li key={index} className={`${isBooked(day) && isToday(day) && style.booked_today} ${isBooked(day) && style.booked} ${isToday(day) && style.today}`}>
           {format(day, "d")}
         </li>
       ))}
     </>
   );
-}
+};
 
 const Calendar = ({
   firstDayOfTheMonth,
@@ -87,30 +93,50 @@ const CalendarWrapper = ({ startDate = new Date() }) => {
   };
 
   return (
-    <div className={style.wrapper}>
-      <header className={style.header}>
-        <p className={style.current_date}>{format(date, "MMM yyyy")}</p>
-        <div className={style.icons}>
-          <button type="button" className={`${style.icon} ${style.prev}`} onClick={() => handleDateChange("prev")}>
-            <i className='bx bx-chevron-left'></i>
-          </button>
-          <button type="button" className={`${style.icon} ${style.next}`} onClick={() => handleDateChange("next")}>
-            <i className='bx bx-chevron-right'></i>
-          </button>
+    <>
+      <div className={style.wrapper}>
+        <header className={style.header}>
+          <p className={style.current_date}>{format(date, "MMM yyyy")}</p>
+          <div className={style.icons}>
+            <button type="button" className={`${style.icon} ${style.prev}`} onClick={() => handleDateChange("prev")}>
+              <i className='bx bx-chevron-left'></i>
+            </button>
+            <button type="button" className={`${style.icon} ${style.next}`} onClick={() => handleDateChange("next")}>
+              <i className='bx bx-chevron-right'></i>
+            </button>
+          </div>
+        </header>
+        <div className={style.calendar}>
+          <ul className={style.weeks}>
+            {DAYS_OF_THE_WEEK.map((week) => (<li key={week.id}>{week.short}</li>))}
+          </ul>
+          <Calendar
+            firstDayOfTheMonth={firstDayOfTheMonth}
+            lastDayOfMonth={lastDayOfMonth}
+            lastDateOfLastMonth={lastDateOfLastMonth}
+            days={days}
+          />
         </div>
-      </header>
-      <div className={style.calendar}>
-        <ul className={style.weeks}>
-          {DAYS_OF_THE_WEEK.map((week) => (<li key={week.id}>{week.short}</li>))}
-        </ul>
-        <Calendar
-          firstDayOfTheMonth={firstDayOfTheMonth}
-          lastDayOfMonth={lastDayOfMonth}
-          lastDateOfLastMonth={lastDateOfLastMonth}
-          days={days}
-        />
       </div>
-    </div>
+      <div className={style.legends}>
+        <div className={style.legend}>
+          <div className={`${style.div} ${style.booking}`}></div>
+          <div className={style.name}>Booking</div>
+        </div>
+        <div className={style.legend}>
+          <div className={`${style.div} ${style.booking_today}`}></div>
+          <div className={style.name}>Today Booking</div>
+        </div>
+        <div className={style.legend}>
+          <div className={`${style.div} ${style.reservation}`}></div>
+          <div className={style.name}>Reservation</div>
+        </div>
+        <div className={style.legend}>
+          <div className={`${style.div} ${style.reservation_today}`}></div>
+          <div className={style.name}>Today Reservation</div>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Calendar/01/data.js
@@ -0,0 +1,6 @@
+export const bookings = [
+  new Date(2024, 1, 4).getTime(),
+  new Date(2024, 1, 7).getTime(),
+  new Date(2024, 1, 12).getTime(),
+  new Date(2024, 1, 15).getTime(),
+];
